c9c3e3c8852381383ed22e0daefa9c518ccf37e1
Abdul-Rahman Hamza <abdulrahmanhamza2205@gmail.com>
2024-07-26 14:59:07 +0300

    add scroller to player list

--- src/tictactoe/PlayerStatusBase.java
+++ src/tictactoe/PlayerStatusBase.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -22,28 +24,22 @@ import org.json.JSONException;
 
 public class PlayerStatusBase extends AnchorPane {
 
+    // UI Components
     protected final Text text;
     protected final Text text0;
     protected final Text text1;
     protected final Text text2;
     protected final Text text3;
-    protected final AnchorPane AncorPane2;
-    protected final Text text4;
-    protected final Text text5;
-    protected final Text text6;
-    protected final Text text7;
-    protected final ImageView ImageViwe1;
-    protected final ImageView ImageViwe2;
-    protected final ImageView ImageViwe3;
-    protected final Button Challenge1Btn;
-    protected final Button Challenge2Btn;
-    protected final Button Challenge3Btn;
+    protected final ScrollPane scrollPane;
+    protected final VBox vBox;
     private final Stage stage;
 
+    // Networking components
     private Socket socket;
     private BufferedReader input;
     private PrintWriter output;
 
+    // Constructor to initialize UI and network connection
     public PlayerStatusBase(Stage stage) {
         this.stage = stage;
         text = new Text();
@@ -51,18 +47,10 @@ public class PlayerStatusBase extends AnchorPane {
         text1 = new Text();
         text2 = new Text();
         text3 = new Text();
-        AncorPane2 = new AnchorPane();
-        text4 = new Text();
-        text5 = new Text();
-        text6 = new Text();
-        text7 = new Text();
-        ImageViwe1 = new ImageView();
-        ImageViwe2 = new ImageView();
-        ImageViwe3 = new ImageView();
-        Challenge1Btn = new Button();
-        Challenge2Btn = new Button();
-        Challenge3Btn = new Button();
+        scrollPane = new ScrollPane();
+        vBox = new VBox(10); // Vertical box with spacing of 10
 
+        // Set dimensions of the AnchorPane
         setMaxHeight(USE_PREF_SIZE);
         setMaxWidth(USE_PREF_SIZE);
         setMinHeight(USE_PREF_SIZE);
@@ -70,6 +58,7 @@ public class PlayerStatusBase extends AnchorPane {
         setPrefHeight(400.0);
         setPrefWidth(600.0);
 
+        // Title text settings
         text.setFill(javafx.scene.paint.Color.valueOf("#04b1b8"));
         text.setLayoutX(206.0);
         text.setLayoutY(65.0);
@@ -94,6 +83,7 @@ public class PlayerStatusBase extends AnchorPane {
         text1.setText("TAC");
         text1.setFont(new Font("Agency FB Bold", 45.0));
 
+        // Labels for player name and score
         text2.setFill(javafx.scene.paint.Color.valueOf("#2A9DB8"));
         text2.setLayoutX(26.0);
         text2.setLayoutY(113.0);
@@ -110,109 +100,27 @@ public class PlayerStatusBase extends AnchorPane {
         text3.setText("SCORE");
         text3.setFont(new Font("Agency FB Bold", 35.0));
 
-        AncorPane2.setLayoutX(110.0);
-        AncorPane2.setLayoutY(121.0);
-        AncorPane2.setPrefHeight(227.0);
-        AncorPane2.setPrefWidth(366.0);
-        AncorPane2.setStyle("-fx-background-color: #2A9DB8;");
+        // ScrollPane to hold the list of players
+        scrollPane.setLayoutX(110.0);
+        scrollPane.setLayoutY(121.0);
+        scrollPane.setPrefHeight(227.0);
+        scrollPane.setPrefWidth(366.0);
+        scrollPane.setContent(vBox);
+        scrollPane.setStyle("-fx-background-color: #2A9DB8;");
+        scrollPane.setFitToWidth(true);
 
-        text4.setLayoutX(41.0);
-        text4.setFill(javafx.scene.paint.Color.valueOf("#ffffff"));
-        text4.setLayoutY(96.0);
-        text4.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        text4.setStrokeWidth(0.0);
-        text4.setText("PLAYER ONE");
-        text4.setFont(new Font("Agency FB Bold", 24.0));
-
-        text5.setLayoutX(39.0);
-        text5.setFill(javafx.scene.paint.Color.valueOf("#ffffff"));
-        text5.setLayoutY(146.0);
-        text5.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        text5.setStrokeWidth(0.0);
-        text5.setText("PLAYER TWO");
-        text5.setFont(new Font("Agency FB Bold", 24.0));
-
-        text6.setLayoutX(39.0);
-        text6.setFill(javafx.scene.paint.Color.valueOf("#ffffff"));
-        text6.setLayoutY(196.0);
-        text6.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        text6.setStrokeWidth(0.0);
-        text6.setText("PLAYER THREE");
-        text6.setFont(new Font("Agency FB Bold", 24.0));
-
-        text7.setLayoutX(284.0);
-        text7.setFill(javafx.scene.paint.Color.valueOf("#ffffff"));
-        text7.setLayoutY(48.0);
-        text7.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        text7.setStrokeWidth(0.0);
-        text7.setText("ACTIVE USERS");
-        text7.setFont(new Font("Agency FB Bold", 24.0));
-
-        ImageViwe1.setFitHeight(34.0);
-        ImageViwe1.setFitWidth(34.0);
-        ImageViwe1.setLayoutX(4.0);
-        ImageViwe1.setLayoutY(70.0);
-        ImageViwe1.setPickOnBounds(true);
-        ImageViwe1.setPreserveRatio(true);
-        ImageViwe1.setImage(new Image(getClass().getResource("person.png").toExternalForm()));
-
-        ImageViwe2.setFitHeight(34.0);
-        ImageViwe2.setFitWidth(34.0);
-        ImageViwe2.setLayoutX(4.0);
-        ImageViwe2.setLayoutY(120.0);
-        ImageViwe2.setPickOnBounds(true);
-        ImageViwe2.setPreserveRatio(true);
-        ImageViwe2.setImage(new Image(getClass().getResource("person.png").toExternalForm()));
-
-        ImageViwe3.setFitHeight(34.0);
-        ImageViwe3.setFitWidth(34.0);
-        ImageViwe3.setLayoutX(4.0);
-        ImageViwe3.setLayoutY(170.0);
-        ImageViwe3.setPickOnBounds(true);
-        ImageViwe3.setPreserveRatio(true);
-        ImageViwe3.setImage(new Image(getClass().getResource("person.png").toExternalForm()));
-
-        Challenge1Btn.setLayoutX(264.0);
-        Challenge1Btn.setLayoutY(70.0);
-        Challenge1Btn.setMnemonicParsing(false);
-        Challenge1Btn.setText("challenge");
-        Challenge1Btn.setFont(new Font("Agency FB Bold", 18.0));
-        Challenge1Btn.setStyle("-fx-background-color: #F4A24C; -fx-text-fill: #ffffff;");
-
-        Challenge2Btn.setLayoutX(264.0);
-        Challenge2Btn.setLayoutY(120.0);
-        Challenge2Btn.setMnemonicParsing(false);
-        Challenge2Btn.setText("challenge");
-        Challenge2Btn.setFont(new Font("Agency FB Bold", 18.0));
-        Challenge2Btn.setStyle("-fx-background-color: #F4A24C; -fx-text-fill: #ffffff;");
-
-        Challenge3Btn.setLayoutX(264.0);
-        Challenge3Btn.setLayoutY(170.0);
-        Challenge3Btn.setMnemonicParsing(false);
-        Challenge3Btn.setText("challenge");
-        Challenge3Btn.setFont(new Font("Agency FB Bold", 18.0));
-        Challenge3Btn.setStyle("-fx-background-color: #F4A24C; -fx-text-fill: #ffffff;");
-
+        // Add UI components to the AnchorPane
         getChildren().add(text);
         getChildren().add(text0);
         getChildren().add(text1);
         getChildren().add(text2);
         getChildren().add(text3);
-        AncorPane2.getChildren().add(text4);
-        AncorPane2.getChildren().add(text5);
-        AncorPane2.getChildren().add(text6);
-        AncorPane2.getChildren().add(text7);
-        AncorPane2.getChildren().add(ImageViwe1);
-        AncorPane2.getChildren().add(ImageViwe2);
-        AncorPane2.getChildren().add(ImageViwe3);
-        AncorPane2.getChildren().add(Challenge1Btn);
-        AncorPane2.getChildren().add(Challenge2Btn);
-        AncorPane2.getChildren().add(Challenge3Btn);
-        getChildren().add(AncorPane2);
+        getChildren().add(scrollPane);
 
         // Connect to the server
         try {
-            socket = new Socket("10.10.11.79", 9081);
+            // Establish socket connection
+            socket = new Socket("192.168.1.106", 9081);
             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             output = new PrintWriter(socket.getOutputStream(), true);
 
@@ -225,26 +133,61 @@ public class PlayerStatusBase extends AnchorPane {
             String response = input.readLine();
             JSONObject data = new JSONObject(response);
             List<String> usersList = Arrays.asList(data.getString("activeUsers").split(","));
-            // Update UI with active users
-            if (usersList.size() > 0) {
-                text4.setText(usersList.get(0));
-            }
-            if (usersList.size() > 1) {
-                text5.setText(usersList.get(1));
-            }
-            if (usersList.size() > 2) {
-                text6.setText(usersList.get(2));
-            }
 
-            Challenge1Btn.setOnAction(event -> challengeUser(usersList.get(0)));
-            Challenge2Btn.setOnAction(event -> challengeUser(usersList.get(1)));
-            Challenge3Btn.setOnAction(event -> challengeUser(usersList.get(2)));
+            // Update UI with active users
+            for (String user : usersList) {
+                vBox.getChildren().add(createPlayerEntry(user));
+            }
 
         } catch (IOException | JSONException e) {
             Logger.getLogger(PlayerStatusBase.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 
+    // Method to create a UI entry for each player
+    private AnchorPane createPlayerEntry(String playerName) {
+        AnchorPane playerPane = new AnchorPane();
+        playerPane.setStyle("-fx-background-color: #2A9DB8;");
+        playerPane.setPrefHeight(50.0);
+        playerPane.setPrefWidth(366.0);
+
+        // Player name text
+        Text playerText = new Text(playerName);
+        playerText.setFill(javafx.scene.paint.Color.valueOf("#ffffff"));
+        playerText.setLayoutX(39.0);
+        playerText.setLayoutY(30.0);
+        playerText.setFont(new Font("Agency FB Bold", 24.0));
+
+        // Player image
+        ImageView playerImage = new ImageView();
+        playerImage.setFitHeight(34.0);
+        playerImage.setFitWidth(34.0);
+        playerImage.setLayoutX(4.0);
+        playerImage.setLayoutY(8.0);
+        playerImage.setPickOnBounds(true);
+        playerImage.setPreserveRatio(true);
+        playerImage.setImage(new Image(getClass().getResource("person.png").toExternalForm()));
+
+        // Challenge button
+        Button challengeButton = new Button("challenge");
+        challengeButton.setLayoutX(264.0);
+        challengeButton.setLayoutY(10.0);
+        challengeButton.setMnemonicParsing(false);
+        challengeButton.setFont(new Font("Agency FB Bold", 18.0));
+        challengeButton.setStyle("-fx-background-color: #F4A24C; -fx-text-fill: #ffffff;");
+
+        // Set action for challenge button
+        challengeButton.setOnAction(event -> challengeUser(playerName));
+
+        // Add components to playerPane
+        playerPane.getChildren().add(playerText);
+        playerPane.getChildren().add(playerImage);
+        playerPane.getChildren().add(challengeButton);
+
+        return playerPane;
+    }
+
+    // Method to send a challenge request to the server
     private void challengeUser(String opponent) {
         try {
             JSONObject request = new JSONObject();
